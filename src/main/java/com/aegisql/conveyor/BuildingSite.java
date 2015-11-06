c7b68358ba1e92b533abfd812d831c98ece280e8
aegisql <mvteplitsky@yahoo.com>
2015-11-05 23:57:51 -0500

    refactoring

--- src/main/java/com/aegisql/conveyor/BuildingSite.java
+++ src/main/java/com/aegisql/conveyor/BuildingSite.java
@@ -91,12 +91,8 @@ public class BuildingSite <K, L, C extends Cart<K, ?, L>, OUT> implements Delaye
 	}
 
 	public void accept(C cart) {
-		L label = null;
-		Object value = null;
-		if(cart != null) {
-			 label = cart.getLabel();
-			 value = cart.getValue();
-		}
+		L label      = cart.getLabel();
+		Object value = cart.getValue();
 		
 		if( label == null || ! (label instanceof SmartLabel) ) {
 			valueConsumer.accept(label, value, builder);
