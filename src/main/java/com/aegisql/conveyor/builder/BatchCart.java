7dd33d2150ba3e460159cecfba7b1680f9625983
aegisql <mvteplitsky@yahoo.com>
2015-11-20 22:16:24 -0500

    batch conveyor

--- src/main/java/com/aegisql/conveyor/builder/BatchCart.java
+++ src/main/java/com/aegisql/conveyor/builder/BatchCart.java
@@ -1,36 +1,46 @@
 package com.aegisql.conveyor.builder;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 
 import com.aegisql.conveyor.Cart;
+import com.aegisql.conveyor.SmartLabel;
 
-public class BatchCart <V> extends
-		Cart<String, V, BatchLabel<V>> {
+public class BatchCart <V> extends Cart<String, V, SmartLabel<BatchCollectingBuilder<V>>> {
 
 	private static final long serialVersionUID = 7358052456949320913L;
 
 	public BatchCart(String k, V v, long ttl, TimeUnit timeUnit) {
-		super(k, v, new BatchLabel<V>(), ttl, timeUnit);
+		super(k, v, BatchCart.<V>getAddLabel(), ttl, timeUnit);
 	}
 
 	public BatchCart(String k, V v, long expiration) {
-		super(k, v, new BatchLabel<V>(), expiration);
+		super(k, v, BatchCart.<V>getAddLabel(), expiration);
 	}
 
 	public BatchCart(String k, V v) {
-		super(k, v, new BatchLabel<V>());
+		super(k, v, BatchCart.<V>getAddLabel());
 	}
 
 	public BatchCart(V v, long ttl, TimeUnit timeUnit) {
-		super("__BATCH__", v, new BatchLabel<V>(), ttl, timeUnit);
+		super("_BATCH_"+v.getClass().getName(), v, BatchCart.<V>getAddLabel(), ttl, timeUnit);
 	}
 
 	public BatchCart(V v, long expiration) {
-		super("__BATCH__", v, new BatchLabel<V>(), expiration);
+		super("_BATCH_"+v.getClass().getName(), v, BatchCart.<V>getAddLabel(), expiration);
 	}
 
 	public BatchCart(V v) {
-		super("__BATCH__", v, new BatchLabel<V>());
+		super("_BATCH_"+v.getClass().getName(), v, BatchCart.<V>getAddLabel() );
 	}
-	
+		
+	private static <V> SmartLabel<BatchCollectingBuilder<V>> getAddLabel() {
+		return new SmartLabel<BatchCollectingBuilder<V>>(){
+			private static final long serialVersionUID = 1L;
+			public BiConsumer<BatchCollectingBuilder<V>, Object> getSetter() {
+				BiConsumer<BatchCollectingBuilder<V>, V> consumer = BatchCollectingBuilder::add;
+				return (BiConsumer<BatchCollectingBuilder<V>, Object>)consumer;
+			}};
+	}
+
 }
---
+++ src/main/java/com/aegisql/conveyor/builder/BatchConveyor.java
@@ -0,0 +1,14 @@
+package com.aegisql.conveyor.builder;
+
+import java.util.List;
+
+import com.aegisql.conveyor.AssemblingConveyor;
+import com.aegisql.conveyor.SmartLabel;
+
+public class BatchConveyor <V> extends AssemblingConveyor<String, SmartLabel<BatchCollectingBuilder<V>>, BatchCart<V>, List<V>> {
+
+	public BatchConveyor() {
+		super();
+	}
+
+}
