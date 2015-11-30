136f888c48739362820176c4709a50dd198969bc
aegisql <mvteplitsky@yahoo.com>
2015-11-29 23:17:25 -0500

    save

--- src/main/java/com/aegisql/conveyor/ChainResult.java
+++ src/main/java/com/aegisql/conveyor/ChainResult.java
@@ -1,5 +1,7 @@
 package com.aegisql.conveyor;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import com.aegisql.conveyor.cart.Cart;
@@ -10,14 +12,29 @@ public class ChainResult<K2, L2, IN2 extends Cart<K2,OUT1,L2>,OUT1> implements C
 	private final Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next;
 	private final ShoppingCart<K2,OUT1,L2> prototype;
 	
-	public ChainResult(Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next, ShoppingCart<K2,OUT1,L2> prototype ) {
-		this.next      = next;
-		this.prototype = prototype;
-	}
+	private long ttl = 0;
+	TimeUnit unit = null;
 	
+	public ChainResult(Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next, ShoppingCart<K2,OUT1,L2> prototype ) {
+		this.next      = Objects.requireNonNull(next);
+		this.prototype = Objects.requireNonNull(prototype);
+	}
+
+	public ChainResult(Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next, ShoppingCart<K2,OUT1,L2> prototype, long ttl, TimeUnit unit ) {
+		this.next      = Objects.requireNonNull(next);
+		this.prototype = Objects.requireNonNull(prototype);
+		this.ttl       = ttl;
+		this.unit      = unit;
+	}
+
 	@Override
 	public void accept(OUT1 t) {
-		Cart<K2, OUT1, L2> cart = prototype.nextCart(t);
+		Cart<K2, OUT1, L2> cart = null;
+		if(unit == null) {
+			cart = prototype.nextCart(t);
+		} else {
+			cart = new ShoppingCart<K2, OUT1, L2>(prototype.getKey(), t, prototype.getLabel(), ttl, unit);
+		}
 		next.add(cart);
 	}
 
