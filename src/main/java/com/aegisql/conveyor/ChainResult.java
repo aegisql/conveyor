5d5f19e1780942d5507c1c0e6de7e66094fc90e1
aegisql <mvteplitsky@yahoo.com>
2015-11-29 23:09:21 -0500

    save

--- src/main/java/com/aegisql/conveyor/ChainResult.java
+++ src/main/java/com/aegisql/conveyor/ChainResult.java
@@ -5,19 +5,19 @@ import java.util.function.Consumer;
 import com.aegisql.conveyor.cart.Cart;
 import com.aegisql.conveyor.cart.ShoppingCart;
 
-public class ChainResult<K, L, IN extends Cart<K,?,L>,OUT> implements Consumer<OUT> {
+public class ChainResult<K2, L2, IN2 extends Cart<K2,OUT1,L2>,OUT1> implements Consumer<OUT1> {
 
-	private final Conveyor<K, L, Cart<K,?,L>, ?> next;
-	private final ShoppingCart<K,?,L> prototype;
+	private final Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next;
+	private final ShoppingCart<K2,OUT1,L2> prototype;
 	
-	public ChainResult(Conveyor<K, L, Cart<K,?,L>, ?> next, ShoppingCart<K,?,L> prototype ) {
+	public ChainResult(Conveyor<K2, L2, Cart<K2,OUT1,L2>, ?> next, ShoppingCart<K2,OUT1,L2> prototype ) {
 		this.next      = next;
 		this.prototype = prototype;
 	}
 	
 	@Override
-	public void accept(OUT t) {
-		Cart<K, OUT, L> cart = prototype.nextCart(t);
+	public void accept(OUT1 t) {
+		Cart<K2, OUT1, L2> cart = prototype.nextCart(t);
 		next.add(cart);
 	}
 
