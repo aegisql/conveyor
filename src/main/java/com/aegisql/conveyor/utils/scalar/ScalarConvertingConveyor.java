d0c289a2e5058a4ee820e22ed80503aadd9d8955
aegisql <mvteplitsky@yahoo.com>
2015-12-05 23:19:22 -0500

    improved interface

--- src/main/java/com/aegisql/conveyor/utils/scalar/ScalarConvertingConveyor.java
+++ src/main/java/com/aegisql/conveyor/utils/scalar/ScalarConvertingConveyor.java
@@ -2,8 +2,10 @@ package com.aegisql.conveyor.utils.scalar;
 
 import com.aegisql.conveyor.AssemblingConveyor;
 import com.aegisql.conveyor.SmartLabel;
+import com.aegisql.conveyor.cart.Cart;
+import com.aegisql.conveyor.cart.ShoppingCart;
 
-public class ScalarConvertingConveyor <K,IN,OUT> extends AssemblingConveyor<K, SmartLabel<ScalarConvertingBuilder<IN,?>>, ScalarCart<K,IN>, OUT> {
+public class ScalarConvertingConveyor <K,IN,OUT> extends AssemblingConveyor<K, SmartLabel<ScalarConvertingBuilder<IN,?>>, Cart<K, IN, SmartLabel<ScalarConvertingBuilder<IN, ?>>>, OUT> {
 
 	public ScalarConvertingConveyor() {
 		super();
