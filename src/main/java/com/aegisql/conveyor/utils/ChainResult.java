4da53cb088012ced6542766c83237092667d499b
Mike Teplitskiy <mvteplitsky@yahoo.com>
2015-12-07 14:33:46 -0500

    cleanup

--- src/main/java/com/aegisql/conveyor/utils/ChainResult.java
+++ src/main/java/com/aegisql/conveyor/utils/ChainResult.java
@@ -13,16 +13,12 @@ import com.aegisql.conveyor.cart.ShoppingCart;
 public class ChainResult<K,IN2 extends Cart<K,?,?>, OUT1> implements Consumer<ProductBin<K,OUT1>> {
 
 	private final Conveyor<K, ?, Cart<K,OUT1,?>, ?> next;
-	private final IN2 prototype;
 	
 	private Function<ProductBin<K,OUT1>,Cart<K,OUT1,?>> cartBuilder;
 	
-	private long ttl = 0;
-	TimeUnit unit = null;
-	
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype ) {
 		this.next      = Objects.requireNonNull(next);
-		this.prototype = Objects.requireNonNull(prototype);
+		Objects.requireNonNull(prototype);
 		
 		cartBuilder = bin -> ((ShoppingCart) prototype).nextCart(bin.product);
 		
@@ -30,9 +26,7 @@ public class ChainResult<K,IN2 extends Cart<K,?,?>, OUT1> implements Consumer<Pr
 
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype, long ttl, TimeUnit unit ) {
 		this.next      = Objects.requireNonNull(next);
-		this.prototype = Objects.requireNonNull(prototype);
-		this.ttl       = ttl;
-		this.unit      = unit;
+		Objects.requireNonNull(prototype);
 
 		cartBuilder = bin -> new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), ttl, unit);
 
