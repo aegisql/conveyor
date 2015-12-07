c36db25693525f1fb76c1dceaa0fccbf9313d89a
Mike Teplitskiy <mvteplitsky@yahoo.com>
2015-12-07 14:32:29 -0500

    Use Function - not finished

--- src/main/java/com/aegisql/conveyor/utils/ChainResult.java
+++ src/main/java/com/aegisql/conveyor/utils/ChainResult.java
@@ -3,6 +3,7 @@ package com.aegisql.conveyor.utils;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import com.aegisql.conveyor.Conveyor;
 import com.aegisql.conveyor.ProductBin;
@@ -14,12 +15,17 @@ public class ChainResult<K,IN2 extends Cart<K,?,?>, OUT1> implements Consumer<Pr
 	private final Conveyor<K, ?, Cart<K,OUT1,?>, ?> next;
 	private final IN2 prototype;
 	
+	private Function<ProductBin<K,OUT1>,Cart<K,OUT1,?>> cartBuilder;
+	
 	private long ttl = 0;
 	TimeUnit unit = null;
 	
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype ) {
 		this.next      = Objects.requireNonNull(next);
 		this.prototype = Objects.requireNonNull(prototype);
+		
+		cartBuilder = bin -> ((ShoppingCart) prototype).nextCart(bin.product);
+		
 	}
 
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype, long ttl, TimeUnit unit ) {
@@ -27,17 +33,14 @@ public class ChainResult<K,IN2 extends Cart<K,?,?>, OUT1> implements Consumer<Pr
 		this.prototype = Objects.requireNonNull(prototype);
 		this.ttl       = ttl;
 		this.unit      = unit;
+
+		cartBuilder = bin -> new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), ttl, unit);
+
 	}
 
 	@Override
 	public void accept(ProductBin<K,OUT1> bin) {
-		ShoppingCart<K, OUT1, ?> cart = null;
-		if(unit == null) {
-			cart = ((ShoppingCart) prototype).nextCart(bin.product);
-		} else {
-			cart = new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), ttl, unit);
-		}
-		next.add(cart);
+		next.add( cartBuilder.apply(bin) );
 	}
 
 }
