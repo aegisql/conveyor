bf971f97fbce24c8b6fbc7a5a35166ec224e72f7
aegisql <mvteplitsky@yahoo.com>
2015-12-07 23:02:31 -0500

    Chain refactoring

--- src/main/java/com/aegisql/conveyor/utils/ChainResult.java
+++ src/main/java/com/aegisql/conveyor/utils/ChainResult.java
@@ -10,26 +10,23 @@ import com.aegisql.conveyor.ProductBin;
 import com.aegisql.conveyor.cart.Cart;
 import com.aegisql.conveyor.cart.ShoppingCart;
 
-public class ChainResult<K,IN2 extends Cart<K,?,?>, OUT1> implements Consumer<ProductBin<K,OUT1>> {
+public class ChainResult<K,IN2 extends Cart<K,OUT1,?>, OUT1> implements Consumer<ProductBin<K,OUT1>> {
 
 	private final Conveyor<K, ?, Cart<K,OUT1,?>, ?> next;
 	
 	private Function<ProductBin<K,OUT1>,Cart<K,OUT1,?>> cartBuilder;
-	
+
+	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, Function<ProductBin<K,OUT1>,Cart<K,OUT1,?>> cartBuilder) {
+		this.next        = Objects.requireNonNull(next);
+		this.cartBuilder = Objects.requireNonNull(cartBuilder);
+	}
+
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype ) {
-		this.next      = Objects.requireNonNull(next);
-		Objects.requireNonNull(prototype);
-		
-		cartBuilder = bin -> ((ShoppingCart) prototype).nextCart(bin.product);
-		
+		this(next, bin -> new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), bin.remainingDelayMsec,TimeUnit.MILLISECONDS));
 	}
 
 	public ChainResult(Conveyor<K, ?, Cart<K,OUT1,?>, ?> next, IN2 prototype, long ttl, TimeUnit unit ) {
-		this.next      = Objects.requireNonNull(next);
-		Objects.requireNonNull(prototype);
-
-		cartBuilder = bin -> new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), ttl, unit);
-
+		this(next,bin -> new ShoppingCart(prototype.getKey(), bin.product, prototype.getLabel(), ttl, unit));
 	}
 
 	@Override
