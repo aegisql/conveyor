a6819e15e9eb7969d6b9462ba525b41496fa5cdb
aegisql <mvteplitsky@yahoo.com>
2015-12-19 12:01:14 -0500

    refactoring

--- src/main/java/com/aegisql/conveyor/utils/parallel/ParallelConveyor.java
+++ src/main/java/com/aegisql/conveyor/utils/parallel/ParallelConveyor.java
@@ -72,16 +72,7 @@ public class ParallelConveyor<K, L, OUT> implements Conveyor<K, L, OUT> {
 	 * @param pf the pf
 	 */
 	public ParallelConveyor( int pf ) {
-		if( pf <=0 ) {
-			throw new IllegalArgumentException("");
-		}
-		this.pf = pf;
-		AssemblingConveyor<K, L, OUT>[] conveyors = new AssemblingConveyor[pf];
-		for(int i = 0; i < pf; i++) {
-			conveyors[i] = new AssemblingConveyor<K, L, OUT>();
-		}
-		this.conveyors = conveyors;
-		this.balancingFunction = key -> key.hashCode() % pf;
+		this(AssemblingConveyor::new,pf);
 	}
 
 	public ParallelConveyor( Supplier<? extends AssemblingConveyor<K, L, OUT>> cs, int pf ) {
