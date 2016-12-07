4b28337b7a74c066993a1097f2917df9c909b3db
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-12-07 16:24:36 -0500

    BuildTester

--- src/main/java/com/aegisql/conveyor/BuildTester.java
+++ src/main/java/com/aegisql/conveyor/BuildTester.java
@@ -3,16 +3,62 @@ package com.aegisql.conveyor;
 import java.util.function.BiPredicate;
 import java.util.function.Supplier;
 
-public class BuildTester<K,L,OUT>  {
+public class BuildTester<K,L,OUT> implements BiPredicate<State<K,L>, Supplier<? extends OUT>> {
 
-	BiPredicate<State<K,L>, Supplier<? extends OUT>> p = (s,b) -> true;
+	private final BiPredicate<State<K,L>, Supplier<? extends OUT>> p;
 	
 	public BuildTester() {
-		
+		this.p = (s,b) -> true;
+	}
+
+	private BuildTester(BiPredicate<State<K,L>, Supplier<? extends OUT>> p) {
+		this.p = p;
+	}
+
+	public BuildTester<K,L,OUT> accepted(int times) {
+		return new BuildTester<K,L,OUT>( p.and( (s,b) -> {
+			return s.previouslyAccepted == times;
+		} ) );
+	}
+
+	public BuildTester<K,L,OUT> accepted(L label) {
+		return new BuildTester<K,L,OUT>( p.and( (s,b) -> {
+			return s.eventHistory.containsKey(label);
+		} ) );
+	}
+
+	public BuildTester<K,L,OUT> accepted(L label, int times) {
+		return new BuildTester<K,L,OUT>( p.and( (s,b) -> {
+			Integer counter = s.eventHistory.get(label);
+			if(counter == null) {
+				return false;
+			} else {
+				return counter == times;
+			}
+		} ) );
 	}
 	
-	public BiPredicate<State<K,L>, Supplier<? extends OUT>> get() {
-		return p;
+	public BuildTester<K,L,OUT> and(BuildTester<K,L,OUT> other) {
+		return new BuildTester<K,L,OUT>(  (s,b)->{
+			return this.test(s, b) && other.test(s, b);
+		}  );
+	}
+
+	public BuildTester<K,L,OUT> andNot(BuildTester<K,L,OUT> other) {
+		return new BuildTester<K,L,OUT>(  (s,b)->{
+			return this.test(s, b) && ! other.test(s, b);
+		}  );
+	}
+
+	public BuildTester<K,L,OUT> or(BuildTester<K,L,OUT> other) {
+		return new BuildTester<K,L,OUT>( (s,b)->{
+			return this.test(s, b) || other.test(s, b);
+		}  );
+	}
+	
+	@Override
+	public boolean test(State<K, L> s, Supplier<? extends OUT> b) {
+		return p.test(s, b);
 	}
 	
 }
--- src/test/java/com/aegisql/conveyor/BuildTesterTest.java
+++ src/test/java/com/aegisql/conveyor/BuildTesterTest.java
@@ -2,6 +2,8 @@ package com.aegisql.conveyor;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -27,11 +29,42 @@ public class BuildTesterTest {
 	}
 
 	@Test
-	public void test() {
-
+	public void testTrivialCase() {
 		BuildTester<String, String, String> bt = new BuildTester<>();
-		assertNotNull(bt.get());
-	
+		assertTrue(bt.test(null,null));
+	}
+
+	@Test
+	public void testAcceptedTimes() {
+		BuildTester<String, String, String> bt1 = new BuildTester<String, String, String>().accepted(2);
+		BuildTester<String, String, String> bt2 = new BuildTester<String, String, String>().accepted(3);
+		State<String, String> st = new State<String, String>("1", 0, 0, 0, 0, 2, null, null);
+		assertTrue(bt1.test(st,null));
+		assertFalse(bt2.test(st,null));
+	}
+
+	@Test
+	public void testAcceptedLabel() {
+		BuildTester<String, String, String> bt1 = new BuildTester<String, String, String>().accepted("A");
+		BuildTester<String, String, String> bt2 = new BuildTester<String, String, String>().accepted("B");
+		State<String, String> st = new State<String, String>("1", 0, 0, 0, 0, 2, new HashMap<String,Integer>(){{
+			put("A",1);
+			put("C",2);
+		}}, null);
+		assertTrue(bt1.test(st,null));
+		assertFalse(bt2.test(st,null));
+	}
+
+	@Test
+	public void testAcceptedLabelTimes() {
+		BuildTester<String, String, String> bt1 = new BuildTester<String, String, String>().accepted("A",1).accepted("C") ;
+		BuildTester<String, String, String> bt2 = new BuildTester<String, String, String>().accepted("C",1);
+		State<String, String> st = new State<String, String>("1", 0, 0, 0, 0, 2, new HashMap<String,Integer>(){{
+			put("A",1);
+			put("C",2);
+		}}, null);
+		assertTrue(bt1.test(st,null));
+		assertFalse(bt2.test(st,null));
 	}
 
 }
