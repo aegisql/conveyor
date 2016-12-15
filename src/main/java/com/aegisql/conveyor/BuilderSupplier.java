daf970f1889f59fb34175d0ec41184a8b666e705
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-12-15 18:32:18 -0500

    Start functional extentions for the Builder Supplier

--- src/main/java/com/aegisql/conveyor/BuilderSupplier.java
+++ src/main/java/com/aegisql/conveyor/BuilderSupplier.java
@@ -8,5 +8,28 @@ import java.util.function.Supplier;
  * @param <T> the generic type
  */
 public interface BuilderSupplier <T> extends Supplier<Supplier<? extends T>> {
+	
+	interface ExpireableBuilderSupplier<T> extends BuilderSupplier<T>, Expireable {};
+	interface TestingBuilderSupplier<T> extends BuilderSupplier<T>, Testing {};
+	interface TestingStateBuilderSupplier<T,K,L> extends BuilderSupplier<T>, TestingState<K,L> {};
+	interface TimingOutBuilderSupplier<T> extends BuilderSupplier<T>, TimeoutAction {};
 
+	default BuilderSupplier <T> expire(long expTime) {
+		
+		BuilderSupplier <T> bse = new ExpireableBuilderSupplier<T>() {
+
+			@Override
+			public Supplier<? extends T> get() {
+				return this.get();
+			}
+
+			@Override
+			public long getExpirationTime() {
+				return 0;
+			}
+		};
+		return bse;
+	}
+	
+	
 }
