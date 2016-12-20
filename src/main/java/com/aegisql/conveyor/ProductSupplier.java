280b4c3644b0d22c2bcd70a0d1ed2ea2bc2b03f8
aegisql <mvteplitsky@yahoo.com>
2016-12-19 23:36:58 -0500

    save

--- src/main/java/com/aegisql/conveyor/ProductSupplier.java
+++ src/main/java/com/aegisql/conveyor/ProductSupplier.java
@@ -1,12 +1,14 @@
 package com.aegisql.conveyor;
 
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 
 @FunctionalInterface
 public interface ProductSupplier<T> extends Supplier<T> {
-	
+	//all possible permutations 
 	interface PE<T>       extends ProductSupplier<T>, Expireable {};
 	interface PT<T>       extends ProductSupplier<T>, Testing {};
 	interface PS<T,K,L>   extends ProductSupplier<T>, TestingState<K,L> {};
@@ -28,7 +30,9 @@ public interface ProductSupplier<T> extends Supplier<T> {
 	default ProductSupplier<T> identity() {
 		return this;
 	}
-	default ProductSupplier<T> expire(final Expireable other) {
+	
+// EXPIRE //	
+	default <K,L> ProductSupplier<T> expire(final Expireable other) {
 		final ProductSupplier<T> ps = this;
 		
 		boolean isE = ps instanceof Expireable;
@@ -39,46 +43,93 @@ public interface ProductSupplier<T> extends Supplier<T> {
 		if(isE) {
 			throw new RuntimeException("Already instance of expireable");
 		}
-		
-		
-		if(isT && isO) {
 			
+		if(isT && isO) {
+			return new PETO<T>(){
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return other.getExpirationTime();
+				}
+				@Override
+				public boolean test() {
+					return ((Testing)ps).test();
+				}
+				@Override
+				public void onTimeout() {
+					((TimeoutAction)ps).onTimeout();
+				}
+			};
 		}
 		if(isS && isO) {
-			
+			return new PESO<T,K,L>(){
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return other.getExpirationTime();
+				}
+				@Override
+				public void onTimeout() {
+					((TimeoutAction)ps).onTimeout();
+				}
+				@Override
+				public boolean test(State<K, L> t) {
+					return ((TestingState<K, L>)ps).test(t);
+				}
+			};			
 		}
 		if(isT) {
 			return new PET<T>() {
 				@Override
 				public T get() {
-					return null;
+					return ps.get();
 				}
 				@Override
 				public long getExpirationTime() {
-					return 0;
+					return other.getExpirationTime();
 				}
 				@Override
 				public boolean test() {
-					return false;
+					return ((Testing)ps).test();
 				}
-			};
-			
+			};			
 		}
 		if(isS) {
+			return new PES<T,K,L>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return other.getExpirationTime();
+				}
+				@Override
+				public boolean test(State<K, L> t) {
+					return ((TestingState<K, L>)ps).test(t);
+				}
+			};
 			
 		}
 		if(isO) {
 			return new PEO<T>() {
 				@Override
 				public T get() {
-					return null;
+					return ps.get();
 				}
 				@Override
 				public long getExpirationTime() {
-					return 0;
+					return other.getExpirationTime();
 				}
 				@Override
 				public void onTimeout() {
+					((TimeoutAction)ps).onTimeout();
 				}
 			};
 		}
@@ -93,14 +144,108 @@ public interface ProductSupplier<T> extends Supplier<T> {
 			}
 		};
 	}	
-
-	default ProductSupplier<T> timeout(final Consumer<ProductSupplier<T>> toAction) {
+// TIMEOUT //
+	default <K,L> ProductSupplier<T> timeout(final Consumer<ProductSupplier<T>> toAction) {
 		final ProductSupplier<T> ps = this;
 		
-		if(this instanceof Expireable) {
-			
+		boolean isE = ps instanceof Expireable;
+		boolean isT = ps instanceof Testing;
+		boolean isS = ps instanceof TestingState;
+		boolean isO = ps instanceof TimeoutAction;
+
+		if(isO) {
+			throw new RuntimeException("Already instance of TimeoutAction");
 		}
 		
+		if(isT && isE) {
+			return new PETO<T>(){
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return ((Expireable)ps).getExpirationTime();
+				}
+				@Override
+				public boolean test() {
+					return ((Testing)ps).test();
+				}
+				@Override
+				public void onTimeout() {
+					toAction.accept(ps);					
+				}
+			};
+		}
+		if(isS && isE) {
+			return new PESO<T,K,L>(){
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return ((Expireable)ps).getExpirationTime();
+				}
+				@Override
+				public void onTimeout() {
+					toAction.accept(ps);					
+				}
+				@Override
+				public boolean test(State<K, L> t) {
+					return ((TestingState<K,L>)ps).test(t);
+				}
+			};			
+		}
+		if(isT) {
+			return new PTO<T>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public boolean test() {
+					return ((Testing)ps).test();
+				}
+				@Override
+				public void onTimeout() {
+					toAction.accept(ps);					
+				}
+			};			
+		}
+		if(isS) {
+			return new PSO<T,K,L>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public boolean test(State<K, L> t) {
+					return ((TestingState<K, L>)ps).test(t);
+				}
+				@Override
+				public void onTimeout() {
+					toAction.accept(ps);					
+				}
+			};
+			
+		}
+		if(isE) {
+			return new PEO<T>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return ((Expireable)ps).getExpirationTime();
+				}
+				@Override
+				public void onTimeout() {
+					toAction.accept(ps);
+				}
+			};
+		}
 		return new PO<T>() {
 			@Override
 			public T get() {
@@ -112,5 +257,164 @@ public interface ProductSupplier<T> extends Supplier<T> {
 			}
 		};
 	}	
+// TESTING //
+	default <K,L> ProductSupplier<T> testing(final Predicate<ProductSupplier<T>> tester) {
+		final ProductSupplier<T> ps = this;
+		
+		boolean isE = ps instanceof Expireable;
+		boolean isT = ps instanceof Testing;
+		boolean isS = ps instanceof TestingState;
+		boolean isO = ps instanceof TimeoutAction;
+
+		if(isT) {
+			throw new RuntimeException("Already instance of Testing");
+		}
+		if(isS) {
+			throw new RuntimeException("Already instance of TestingState");
+		}
+		
+		if(isO && isE) {
+			return new PETO<T>(){
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return ((Expireable)ps).getExpirationTime();
+				}
+				@Override
+				public boolean test() {
+					return tester.test(ps);
+				}
+				@Override
+				public void onTimeout() {
+					((TimeoutAction)ps).onTimeout();					
+				}
+			};
+		}
+		if(isO) {
+			return new PTO<T>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public boolean test() {
+					return tester.test(ps);
+				}
+				@Override
+				public void onTimeout() {
+					((TimeoutAction)ps).onTimeout();
+				}
+			};			
+		}
+		if(isE) {
+			return new PET<T>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public long getExpirationTime() {
+					return ((Expireable)ps).getExpirationTime();
+				}
+				@Override
+				public boolean test() {
+					return tester.test(ps);
+				}
+			};
+		}
+		return new PT<T>() {
+			@Override
+			public T get() {
+				return ps.get();
+			}
+			@Override
+			public boolean test() {
+				return tester.test(ps);
+			}
+		};
+	}	
+
+	// TESTING STATE//
+		default <K,L> ProductSupplier<T> testingState(final BiPredicate<State<K,L>,ProductSupplier<T>> tester) {
+			final ProductSupplier<T> ps = this;
+			
+			boolean isE = ps instanceof Expireable;
+			boolean isT = ps instanceof Testing;
+			boolean isS = ps instanceof TestingState;
+			boolean isO = ps instanceof TimeoutAction;
+
+			if(isT) {
+				throw new RuntimeException("Already instance of Testing");
+			}
+			if(isS) {
+				throw new RuntimeException("Already instance of TestingState");
+			}
+			
+			if(isO && isE) {
+				return new PESO<T,K,L>(){
+					@Override
+					public T get() {
+						return ps.get();
+					}
+					@Override
+					public long getExpirationTime() {
+						return ((Expireable)ps).getExpirationTime();
+					}
+					@Override
+					public boolean test(State<K,L> t) {
+						return tester.test(t, ps);
+					}
+					@Override
+					public void onTimeout() {
+						((TimeoutAction)ps).onTimeout();
+					}
+				};
+			}
+			if(isO) {
+				return new PSO<T,K,L>() {
+					@Override
+					public T get() {
+						return ps.get();
+					}
+					@Override
+					public boolean test(State<K,L> t) {
+						return tester.test(t, ps);
+					}
+					@Override
+					public void onTimeout() {
+						((TimeoutAction)ps).onTimeout();
+					}
+				};			
+			}
+			if(isE) {
+				return new PES<T,K,L>() {
+					@Override
+					public T get() {
+						return ps.get();
+					}
+					@Override
+					public long getExpirationTime() {
+						return ((Expireable)ps).getExpirationTime();
+					}
+					@Override
+					public boolean test(State<K,L> t) {
+						return tester.test(t, ps);
+					}
+				};
+			}
+			return new PS<T,K,L>() {
+				@Override
+				public T get() {
+					return ps.get();
+				}
+				@Override
+				public boolean test(State<K,L> t) {
+					return tester.test(t, ps);
+				}
+			};
+		}	
 
 }
