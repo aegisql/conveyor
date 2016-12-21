1c10d96084f014bb1bd062e262cddac4ecfd300b
Mikhail_Teplitskiy <Mikhail_Teplitskiy@epam.com>
2016-12-21 16:07:45 -0500

    Ideas about smart label interface extensions

--- src/main/java/com/aegisql/conveyor/SmartLabel.java
+++ src/main/java/com/aegisql/conveyor/SmartLabel.java
@@ -5,6 +5,7 @@ package com.aegisql.conveyor;
 
 import java.io.Serializable;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 // TODO: Auto-generated Javadoc
@@ -26,8 +27,44 @@ public interface SmartLabel<B> extends Serializable, Supplier<BiConsumer<B, Obje
 	@Override
 	BiConsumer<B, Object> get();
 	
+	static <B> SmartLabel<B> of(BiConsumer<B, Object> method) {
+		return ()->method;
+	}
+	static <B> SmartLabel<B> of(Consumer<B> method) {
+		return () -> {
+			return (b,phony) -> method.accept(b);
+		};
+	}
 	default SmartLabel<B> identity() {
 		return this;
+	}
+	default <T> SmartLabel<B> intercept(Class<T> clas, Consumer<T> interceptor) {
+		SmartLabel<B> sl = this;
+		return ()->(b,o)->{
+			if( o != null ) {
+				if( clas.isAssignableFrom(o.getClass())) {
+					interceptor.accept((T)o);
+				} else {
+					sl.get().accept(b,o);					
+				}
+			} else {
+				sl.get().accept(b,o);
+			}
+		};
+	}
+	default <T> SmartLabel<B> intercept(Class<T> clas, BiConsumer<B,T> interceptor) {
+		SmartLabel<B> sl = this;
+		return ()->(b,o)->{
+			if( o != null ) {
+				if( clas.isAssignableFrom(o.getClass())) {
+					interceptor.accept(b,(T)o);
+				} else {
+					sl.get().accept(b,o);					
+				}
+			} else {
+				sl.get().accept(b,o);
+			}
+		};
 	}
 	
 }
