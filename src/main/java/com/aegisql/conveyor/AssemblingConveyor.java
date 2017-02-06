6b1edb9f14fb2b66fcc4a36bccd5a58bfa7b9149
aegisql <mvteplitsky@yahoo.com>
2017-02-06 00:08:25 -0500

    organized imports

--- src/main/java/com/aegisql/conveyor/AssemblingConveyor.java
+++ src/main/java/com/aegisql/conveyor/AssemblingConveyor.java
@@ -4,12 +4,8 @@
 package com.aegisql.conveyor;
 
 import java.lang.management.ManagementFactory;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
