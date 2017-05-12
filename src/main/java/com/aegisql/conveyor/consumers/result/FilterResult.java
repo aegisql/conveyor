781f40b282b4d1788e2640667c77670d57263ea8
aegisql <mvteplitsky@yahoo.com>
2017-05-12 19:36:17 -0400

    added filter result

---
+++ src/main/java/com/aegisql/conveyor/consumers/result/FilterResult.java
@@ -0,0 +1,25 @@
+package com.aegisql.conveyor.consumers.result;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import com.aegisql.conveyor.ProductBin;
+
+public class FilterResult <K,V> implements Consumer<ProductBin<K,V>>{
+
+	private final Predicate<ProductBin<K,V>> filter;
+	private final Consumer<ProductBin<K,V>> action;
+	
+	public FilterResult(Predicate<ProductBin<K,V>> filter, Consumer<ProductBin<K,V>> action) {
+		this.filter = filter;
+		this.action = action;
+	}
+	
+	@Override
+	public void accept(ProductBin<K, V> t) {
+		if( filter.test(t) ) {
+			action.accept(t);
+		}
+	}
+
+}
