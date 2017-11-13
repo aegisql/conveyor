8c17b9f68cf331684b0915fc9f3a9579202efced
aegisql <mvteplitsky@yahoo.com>
2017-11-12 22:24:48 -0500

    test

--- conveyor-persistence/conveyor-persistence-jdbc/src/test/java/com/aegisql/conveyor/persistence/archive/FileArchiverTest.java
+++ conveyor-persistence/conveyor-persistence-jdbc/src/test/java/com/aegisql/conveyor/persistence/archive/FileArchiverTest.java
@@ -3,6 +3,9 @@ package com.aegisql.conveyor.persistence.archive;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -14,9 +17,11 @@ import org.junit.Test;
 
 import com.aegisql.conveyor.cart.Cart;
 import com.aegisql.conveyor.cart.ShoppingCart;
+import com.aegisql.conveyor.persistence.converters.CartToBytesConverter;
 import com.aegisql.conveyor.persistence.converters.ConverterAdviser;
 import com.aegisql.conveyor.persistence.core.harness.PersistTestImpl;
 import com.aegisql.conveyor.persistence.jdbc.impl.derby.archive.FileArchiver;
+import com.aegisql.conveyor.persistence.utils.CartInputStream;
 import com.aegisql.conveyor.persistence.utils.DataSize;
 
 public class FileArchiverTest {
@@ -87,7 +92,7 @@ public class FileArchiverTest {
 	}
 
 	@Test
-	public void FileArchiverTest() {
+	public void FileArchiverTest() throws Exception {
 		PersistTestImpl p = new PersistTestImpl();
 		BinaryLogConfiguration blc = BinaryLogConfiguration.builder()
 				.maxFileSize("180")
@@ -115,8 +120,70 @@ public class FileArchiverTest {
 		File f1 = new File("./part.blog");
 		assertTrue(f1.exists());
 		assertTrue(f1.length()<=180);
+		
+		FileInputStream fis = new FileInputStream(f1);
+		CartInputStream<Integer, ?> cis = new CartInputStream<>(new CartToBytesConverter<>(adviser),fis);
+		
+		ArrayList<Cart> carts = new ArrayList<>();
+		Cart c = null;
+		do {
+			c = cis.readCart();
+			System.out.println("read cart: "+c);
+			carts.add(c);
+		} while(c != null);
+		
+		assertNotNull(carts.get(0));
+		
 		f1.delete();
 		
 	}
-	
+
+	@Test
+	public void FileArchiverExpiredTest() throws Exception {
+		PersistTestImpl p = new PersistTestImpl();
+		BinaryLogConfiguration blc = BinaryLogConfiguration.builder()
+				.partTableName("expired")
+				.maxFileSize("180")
+				.zipFile(false)
+				.build();
+		ConverterAdviser<?> adviser = new ConverterAdviser<>();
+		FileArchiver<Integer> a = new FileArchiver<>(Integer.class, "expired", "completed", blc, adviser, new DoNothingArchiver<Integer>());
+		a.setPersistence(p);
+		Cart<Integer,String,String> c1 = new ShoppingCart<Integer, String, String>(1, "v1", "l1");
+		Cart<Integer,String,String> c2 = new ShoppingCart<Integer, String, String>(1, "v2", "l1");
+		Cart<Integer,String,String> c3 = new ShoppingCart<Integer, String, String>(1, "v3", "l1");
+		Cart<Integer,String,String> c4 = new ShoppingCart<Integer, String, String>(1, "v4", "l1",System.currentTimeMillis()-1);
+
+		p.savePart(p.nextUniquePartId(), c1);
+		p.savePart(p.nextUniquePartId(), c2);
+		p.savePart(p.nextUniquePartId(), c3);
+		p.savePart(p.nextUniquePartId(), c4);
+		
+		Collection<Long> ids = p.getAllPartIds(1);
+		assertNotNull(ids);
+		assertEquals(4, ids.size());
+		
+		a.archiveExpiredParts(null);
+		
+		File f1 = new File("./expired.blog");
+		assertTrue(f1.exists());
+		assertTrue(f1.length()<=180);
+		
+		FileInputStream fis = new FileInputStream(f1);
+		CartInputStream<Integer, ?> cis = new CartInputStream<>(new CartToBytesConverter<>(adviser),fis);
+		
+		ArrayList<Cart> carts = new ArrayList<>();
+		Cart c = null;
+		do {
+			c = cis.readCart();
+			System.out.println("read cart: "+c);
+			carts.add(c);
+		} while(c != null);
+		
+		assertNotNull(carts.get(0));
+		
+		f1.delete();
+		
+	}
+
 }
