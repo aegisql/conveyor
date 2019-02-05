1676be3a8fe6a4de877028ba34e7e1eda38889d4
aegisql <mvteplitsky@yahoo.com>
2019-02-04 22:56:46 -0500

    restored file

---
+++ conveyor-persistence/conveyor-persistence-jdbc/src/main/java/com/aegisql/conveyor/persistence/jdbc/engine/PostgresqlEngine.java
@@ -0,0 +1,86 @@
+package com.aegisql.conveyor.persistence.jdbc.engine;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.aegisql.conveyor.persistence.core.PersistenceException;
+
+public class PostgresqlEngine <K> extends GenericEngine<K> {
+	public PostgresqlEngine(Class<K> keyClass) {
+		super(
+				keyClass,
+				"org.postgresql.Driver",
+				"jdbc:postgresql://{host}:{port}/",
+				"jdbc:postgresql://{host}:{port}/{database}",
+				"jdbc:postgresql://{host}:{port}/{database}?currentSchema={schema}",
+				"jdbc:postgresql://{host}:{port}/{database}?currentSchema={schema}"
+				);
+		setPort(5432);
+		setHost("localhost");
+		setDatabase("conveyor_db");
+		setSchema("conveyor_db");
+		setField(CART_VALUE, "BYTEA");
+		setField(CREATION_TIME, "TIMESTAMP");
+		setField(EXPIRATION_TIME, "TIMESTAMP");
+
+	}
+	
+	@Override
+	public boolean databaseExists(String database) {
+		AtomicBoolean res = new AtomicBoolean(false);
+		String query = "SELECT 1 FROM pg_catalog.pg_database WHERE lower(datname) = lower(?)";
+
+		connectAndDo(connectionUrlTemplateForInitDatabase, con->{
+			try(PreparedStatement st = con.prepareStatement(query)) {
+				st.setString(1, database);
+				ResultSet rs = st.executeQuery();
+				res.set(rs.next());
+			} catch (SQLException e) {
+				throw new PersistenceException(e);
+			}
+		});
+		return res.get();
+	}
+
+	@Override
+	public boolean partTableIndexExists(String partTable) {
+		AtomicBoolean res = new AtomicBoolean(false);
+		String query = "SELECT 1 FROM pg_catalog. pg_indexes WHERE lower(indexname) = lower(?)";
+
+		connectAndDo(connectionUrlTemplateForInitTablesAndIndexes, con->{
+			try(PreparedStatement st = con.prepareStatement(query)) {
+				st.setString(1, partTable+"_IDX");
+				ResultSet rs = st.executeQuery();
+				res.set(rs.next());
+			} catch (SQLException e) {
+				throw new PersistenceException(e);
+			}
+		});
+		return res.get();
+	}
+
+	@Override
+	protected String getEngineSpecificExpirationTimeRange() {
+		return "EXPIRATION_TIME > '1970-01-01 00:00:01' AND EXPIRATION_TIME < CURRENT_TIMESTAMP";
+	}
+	
+	@Override
+	public void setUser(String user) {
+		super.setUser(user);
+		if(notEmpty(user)) {
+			setProperty("user", user);
+		}
+	}
+
+	@Override
+	public void setPassword(String password) {
+		super.setPassword(password);
+		if(notEmpty(password)) {
+			setProperty("password", password);
+		}
+	}
+	
+	
+}
