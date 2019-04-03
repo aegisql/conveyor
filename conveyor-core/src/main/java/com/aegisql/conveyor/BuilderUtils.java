21539555d3c2b9f7b5e685552a3bef3b6dda640d
aegisql <mvteplitsky@yahoo.com>
2019-04-03 00:42:55 -0400

    save

--- conveyor-core/src/main/java/com/aegisql/conveyor/BuilderUtils.java
+++ conveyor-core/src/main/java/com/aegisql/conveyor/BuilderUtils.java
@@ -1,96 +1,127 @@
 package com.aegisql.conveyor;
 
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
-public class BuilderUtils<K,L,OUT,B> {
+public class BuilderUtils<K, L, OUT, B> {
 
 
-
-    private class ProductSupplierSimple implements Supplier<OUT>, Wrapped<B> {
+    private class ProductSupplierSimple implements Supplier<OUT>, Wrapped<B>, TimeoutAction {
         Supplier<B> builderSupplier;
         B builder;
+
         ProductSupplierSimple(B pb) {
             this.builder = pb;
         }
+
         @Override
         public OUT get() {
             return productSupplier.apply(builder);
         }
+
         @Override
         public B unwrap() {
             return builder;
         }
-    }
 
-    private class ProductSupplierTesting extends ProductSupplierSimple implements Testing {
-        ProductSupplierTesting(B pb) {
-            super(pb);
-        }
         @Override
-        public boolean test() {
-            return testingPredicate.test(builder);
+        public void onTimeout() {
+            timeoutActon.accept(builder);
         }
     }
 
-    private class ProductSupplierTestingState extends ProductSupplierSimple implements TestingState<K,L> {
+    private class ProductSupplierTestingState extends ProductSupplierSimple implements TestingState<K, L> {
         ProductSupplierTestingState(B pb) {
             super(pb);
         }
 
         @Override
         public boolean test(State<K, L> state) {
-            return stateTester.test(state,this.unwrap());
+            return stateTester.test(state, this.unwrap());
         }
     }
 
     private final Conveyor<K, L, OUT> conveyor;
     private final Supplier<B> builderSupplier;
-    private final Function<B,OUT> productSupplier;
-    private final Predicate<B> testingPredicate;
-    private final BiPredicate<State<K,L>, B> stateTester;
+    private final Function<B, OUT> productSupplier;
+    private final BiPredicate<State<K, L>, B> stateTester;
+    private final BiPredicate<State<K, L>, B> timeoutStateTester;
+    private final Consumer<B> timeoutActon;
 
     private BuilderUtils(
             Conveyor<K, L, OUT> conveyor,
             Supplier<B> builderSupplier,
-            Function<B,OUT> productSupplier,
-            Predicate<B> testingPredicate,
-            BiPredicate<State<K,L>, B> stateTester
+            Function<B, OUT> productSupplier,
+            BiPredicate<State<K, L>, B> stateTester,
+            BiPredicate<State<K, L>, B> timeoutStateTester,
+            Consumer<B> timeoutActon
     ) {
         this.conveyor = conveyor;
         this.builderSupplier = builderSupplier;
         this.productSupplier = productSupplier;
-        this.testingPredicate = testingPredicate;
         this.stateTester = stateTester;
+        this.timeoutStateTester = timeoutStateTester;
+        this.timeoutActon = timeoutActon;
     }
 
-    public static <K,L,OUT> BuilderUtils<K,L,OUT,?> forConveyor(Conveyor<K,L,OUT> conveyor) {
-        return new BuilderUtils<>(conveyor,null,null,null, null);
+    public static <K, L, OUT> BuilderUtils<K, L, OUT, ?> forConveyor(Conveyor<K, L, OUT> conveyor) {
+        return new BuilderUtils<>(conveyor, null, null, null, null, b->{});
     }
 
-    public <B> BuilderUtils<K,L,OUT,B> builderSupplier(Supplier<B> bs) {
-        return new BuilderUtils(conveyor,bs,productSupplier,testingPredicate,stateTester);
+    public <B> BuilderUtils<K, L, OUT, B> builderSupplier(Supplier<B> bs) {
+        return new BuilderUtils(conveyor, bs, productSupplier, stateTester, timeoutStateTester, timeoutActon);
     }
 
-    public BuilderUtils<K,L,OUT,B> productSupplier(Function<B,OUT> ps) {
-        return new BuilderUtils<>(conveyor,builderSupplier,ps,testingPredicate,stateTester);
+    public BuilderUtils<K, L, OUT, B> productSupplier(Function<B, OUT> ps) {
+        return new BuilderUtils<>(conveyor, builderSupplier, ps, stateTester, timeoutStateTester, timeoutActon);
     }
 
-    public BuilderUtils<K,L,OUT,B> tester(Predicate<B> tester) {
-        return new BuilderUtils<>(conveyor,builderSupplier,productSupplier,tester,stateTester);
+    public BuilderUtils<K, L, OUT, B> tester(Predicate<B> tester) {
+        return tester((state, b) -> tester.test(b));
     }
 
-    public BuilderUtils<K,L,OUT,B> tester(BiPredicate<State<K,L>, B> stTester) {
-        return new BuilderUtils<>(conveyor,builderSupplier,productSupplier,testingPredicate,stateTester);
+    public BuilderUtils<K, L, OUT, B> tester(BiPredicate<State<K, L>, B> stTester) {
+        BiPredicate<State<K, L>, B> newTester = null;
+        if (timeoutStateTester != null) {
+            newTester = (state, b) -> {
+                boolean expired = state.builderExpiration > 0 && state.builderExpiration - System.currentTimeMillis() <= 0;
+                if (expired) {
+                    return stTester.test(state, b) || timeoutStateTester.test(state, b);
+                } else {
+                    return stTester.test(state, b);
+                }
+            };
+        } else {
+            newTester = stTester;
+        }
+        return new BuilderUtils<>(conveyor, builderSupplier, productSupplier, newTester, timeoutStateTester, timeoutActon);
+    }
+
+    public BuilderUtils<K, L, OUT, B> testerOnTimeout(Predicate<B> tTester) {
+        return testerOnTimeout((state,b)->tTester.test(b));
+    }
+
+    public BuilderUtils<K, L, OUT, B> testerOnTimeout(BiPredicate<State<K, L>, B> tStTester) {
+        BiPredicate<State<K, L>, B> newTester = stateTester;
+        if (newTester != null) {
+            newTester = (state, b) -> {
+                boolean expired = state.builderExpiration > 0 && state.builderExpiration - System.currentTimeMillis() <= 0;
+                if (expired) {
+                    return tStTester.test(state, b) || stateTester.test(state, b);
+                } else {
+                    return stateTester.test(state, b);
+                }
+            };
+        }
+        return new BuilderUtils<>(conveyor, builderSupplier, productSupplier, newTester, tStTester, timeoutActon);
+    }
+
+    public BuilderUtils<K, L, OUT, B> onTimeout(Consumer<B> timeoutActon) {
+        return new BuilderUtils<>(conveyor, builderSupplier, productSupplier, stateTester, timeoutStateTester, timeoutActon);
     }
 
     public void setBuilderSupplier() {
-        if(stateTester != null) {
-            conveyor.setBuilderSupplier(()-> new ProductSupplierTestingState(builderSupplier.get()));
-        } else if(testingPredicate != null) {
-            conveyor.setBuilderSupplier(()-> new ProductSupplierTesting(builderSupplier.get()));
+        if (stateTester != null) {
+            conveyor.setBuilderSupplier(() -> new ProductSupplierTestingState(builderSupplier.get()));
         } else {
             conveyor.setBuilderSupplier(() -> new ProductSupplierSimple(builderSupplier.get()));
         }
--- conveyor-core/src/test/java/com/aegisql/conveyor/ProductBuilderTest.java
+++ conveyor-core/src/test/java/com/aegisql/conveyor/ProductBuilderTest.java
@@ -1,8 +1,10 @@
 package com.aegisql.conveyor;
 
 import com.aegisql.conveyor.consumers.result.LastResultReference;
+import com.aegisql.conveyor.consumers.scrap.LastScrapReference;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -14,6 +16,7 @@ public class ProductBuilderTest {
     public void pocTest() {
 
         LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
         Supplier<StringBuilder> sup = StringBuilder::new;
         Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
 
@@ -27,20 +30,21 @@ public class ProductBuilderTest {
         ac.setName("pocTest");
 
         BuilderUtils.forConveyor(ac)
-                .builderSupplier(StringBuilder::new)
+                .builderSupplier(()->new StringBuilder("INIT "))
                 .productSupplier(sb->sb.toString())
                 .setBuilderSupplier();
 
 
         ac.setReadinessEvaluator(Conveyor.getTesterFor(ac).accepted(DONE));
         ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
         ac.part().id(1).label(APPEND).value("A").place();
         ac.part().id(1).label(APPEND).value(" ").place();
-        ac.part().id(1).label(APPEND).value("B").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
         ac.part().id(1).label(DONE).place();
         ac.completeAndStop().join();
-        System.err.println(res);
-        assertEquals("A B",res.getCurrent());
+        System.err.println(res+"\n"+scrap);
+        assertEquals("INIT A B",res.getCurrent());
     }
 
 
@@ -48,6 +52,7 @@ public class ProductBuilderTest {
     public void pocTestTesting() {
 
         LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
         Supplier<StringBuilder> sup = StringBuilder::new;
         Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
 
@@ -65,12 +70,13 @@ public class ProductBuilderTest {
 
 
         ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
         ac.part().id(1).label(APPEND).value("A").place();
         ac.part().id(1).label(APPEND).value(" ").place();
         ac.part().id(1).label(APPEND).value("B").place();
-        ac.part().id(1).label(APPEND).value(".").place();
+        ac.part().id(1).label(APPEND).value(".").place().join();
         ac.completeAndStop().join();
-        System.err.println(res);
+        System.err.println(res+"\n"+scrap);
         assertEquals("A B.",res.getCurrent());
     }
 
@@ -78,6 +84,7 @@ public class ProductBuilderTest {
     public void pocTestState() {
 
         LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
         Supplier<StringBuilder> sup = StringBuilder::new;
         Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
 
@@ -97,13 +104,217 @@ public class ProductBuilderTest {
 
         ac.setReadinessEvaluator(Conveyor.getTesterFor(ac).accepted(DONE));
         ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
         ac.part().id(1).label(APPEND).value("A").place();
         ac.part().id(1).label(APPEND).value(" ").place();
-        ac.part().id(1).label(APPEND).value("B").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
         ac.part().id(1).label(DONE).place();
         ac.completeAndStop().join();
-        System.err.println(res);
+        System.err.println(res+"\n"+scrap);
         assertEquals("A B",res.getCurrent());
     }
+
+
+    @Test
+    public void pocTimeoutTesting() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutTesting");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .tester(sb->sb.charAt(sb.length()-1)=='.')
+                .testerOnTimeout(b->true)
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("A").place();
+        ac.part().id(1).label(APPEND).value(" ").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("A B",res.getCurrent());
+    }
+
+    @Test
+    public void pocTimeoutStateTesting() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutStateTesting");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .tester(sb->sb.charAt(sb.length()-1)=='.')
+                .testerOnTimeout((s,b)->true)
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("A").place();
+        ac.part().id(1).label(APPEND).value(" ").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("A B",res.getCurrent());
+    }
+
+    @Test
+    public void pocTimeoutTestingRev() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutTestingRev");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .testerOnTimeout(b->true)
+                .tester(sb->sb.charAt(sb.length()-1)=='.')
+                .onTimeout(sb->sb.append(" DEF"))
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("A").place();
+        ac.part().id(1).label(APPEND).value(" ").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("A B DEF",res.getCurrent());
+    }
+
+    @Test
+    public void pocTimeoutStateTestingRev() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutStateTestingRev");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .testerOnTimeout((s,b)->true)
+                .tester(sb->sb.charAt(sb.length()-1)=='.')
+                .onTimeout(sb->sb.append(" XYZ"))
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("A").place();
+        ac.part().id(1).label(APPEND).value(" ").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("A B XYZ",res.getCurrent());
+    }
+
+    @Test
+    public void pocTimeoutStateStateTestingRev() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutStateTestingRev");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .testerOnTimeout((s,b)->true)
+                .tester((state,sb)->sb.charAt(sb.length()-1)=='.')
+                .onTimeout(sb->sb.append(" XYZ"))
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("A").place();
+        ac.part().id(1).label(APPEND).value(" ").place();
+        ac.part().id(1).label(APPEND).value("B").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("A B XYZ",res.getCurrent());
+    }
+
+    @Test
+    public void pocTimeoutSeDefValue() throws InterruptedException {
+
+        LastResultReference<Integer,String> res = new LastResultReference<>();
+        LastScrapReference<Integer> scrap = new LastScrapReference<>();
+        Supplier<StringBuilder> sup = StringBuilder::new;
+        Function<StringBuilder,String> prodSupplier = StringBuilder::toString;
+
+
+        SmartLabel<Wrapped<StringBuilder>> APPEND = SmartLabel.of("APPEND",(stringBuilder, o) -> stringBuilder.unwrap().append(o));
+
+        AssemblingConveyor<Integer,SmartLabel<Wrapped<StringBuilder>>,String> ac = new AssemblingConveyor<>();
+        ac.setName("pocTimeoutSeDefValue");
+        ac.setDefaultBuilderTimeout(Duration.ofMillis(100));
+        ac.setIdleHeartBeat(Duration.ofMillis(10));
+        ac.setReadinessEvaluator(sb->{
+            return ((Wrapped<StringBuilder>)sb).unwrap().length() > 0;
+        });
+        BuilderUtils.forConveyor(ac)
+                .builderSupplier(StringBuilder::new)
+                .productSupplier(sb->sb.toString())
+                .onTimeout(sb->sb.append("DEF VAL"))
+                .setBuilderSupplier();
+
+
+        ac.resultConsumer(res).set();
+        ac.scrapConsumer(scrap).set();
+        ac.part().id(1).label(APPEND).value("").place().join();
+        ac.completeAndStop().join();
+        System.err.println(res+"\n"+scrap);
+        assertEquals("DEF VAL",res.getCurrent());
+    }
+
 
 }
