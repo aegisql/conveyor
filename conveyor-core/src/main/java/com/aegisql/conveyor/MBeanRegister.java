6007f533ce6ad00d0c3b284826333f829f0df1c8
aegisql <mvteplitsky@yahoo.com>
2019-06-17 23:54:05 -0400

    docs

--- conveyor-core/src/main/java/com/aegisql/conveyor/MBeanRegister.java
+++ conveyor-core/src/main/java/com/aegisql/conveyor/MBeanRegister.java
@@ -7,8 +7,14 @@ import java.lang.management.ManagementFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The singleton MBean register.
+ */
 enum MBeanRegister {
 
+    /**
+     * Mbean m bean register.
+     */
     MBEAN;
 
     private final MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
@@ -18,6 +24,12 @@ enum MBeanRegister {
         return "com.aegisql.conveyor:type="+name;
     }
 
+    /**
+     * By name conveyor.
+     *
+     * @param name the name
+     * @return the conveyor
+     */
     public Conveyor byName(String name) {
         try {
             String type = getType(name);
@@ -33,36 +45,51 @@ enum MBeanRegister {
         }
     }
 
+    /**
+     * Register object name.
+     *
+     * @param conveyor    the conveyor
+     * @param mbeanObject the mbean object
+     * @return the object name
+     */
     public ObjectName register(Conveyor conveyor, Object mbeanObject) {
         try {
             Class mBeanInterface = conveyor.mBeanInterface();
             String type = getType(conveyor.getName());
-            knownConveyors.put(type,conveyor);
             if(mBeanInterface != null) {
                 Object mbean = new StandardMBean(mbeanObject, mBeanInterface, false);
                 ObjectName objectName = new ObjectName(type);
                 synchronized (mBeanServer) {
                     unRegister(conveyor.getName());
                     mBeanServer.registerMBean(mbean, objectName);
+                    knownConveyors.put(type,conveyor);
                 }
                 return objectName;
             } else {
+                knownConveyors.put(type,conveyor);
                 return null;
             }
+        } catch( ConveyorRuntimeException cre) {
+          throw cre;
         } catch (Exception e) {
             throw new ConveyorRuntimeException("Error registering conveyor " + conveyor.getName(), e);
         }
     }
 
+    /**
+     * Un register.
+     *
+     * @param name the name
+     */
     public void unRegister(String name) {
         try {
             String type = getType(name);
             ObjectName objectName = new ObjectName(type);
+            knownConveyors.remove(type);
             synchronized (mBeanServer) {
                 if (mBeanServer.isRegistered(objectName)) {
                     Conveyor.LOG.warn("Unregister existing mbean with name {}", objectName);
                     mBeanServer.unregisterMBean(objectName);
-                    knownConveyors.remove(type);
                 }
             }
         } catch (Exception e) {
