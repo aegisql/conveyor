085bab0312cd617c4d5b5ed6691546300fafb6a8
aegisql <mvteplitsky@yahoo.com>
2022-06-22 18:05:24 -0400

    tests

--- conveyor-core/src/test/java/com/aegisql/conveyor/utils/KeyValueTest.java
+++ conveyor-core/src/test/java/com/aegisql/conveyor/utils/KeyValueTest.java
@@ -2,12 +2,15 @@ package com.aegisql.conveyor.utils;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
 import static org.junit.Assert.assertEquals;
 
 public class KeyValueTest {
 
     @Test
     public void basicTest() {
+        new KeyValue<>();
         var a100 = new KeyValue<String,Integer>("A",100);
         var a200 = a100.value(200);
         var b200 = a200.key("B");
@@ -17,7 +20,7 @@ public class KeyValueTest {
         assertEquals(Integer.valueOf(100),a100.value);
         assertEquals(Integer.valueOf(200),a200.value);
         assertEquals(Integer.valueOf(200),b200.value);
-
+        assertEquals(Objects.hash("A",100),a100.hashCode());
         assertEquals(a100,new KeyValue<String,Integer>("A",100));
         System.out.println(a100);
     }
