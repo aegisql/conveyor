ed4358bba855aeefd8a831996509929ce03fbced
mike <mvteplitsky@yahoo.com>
2026-03-10 19:00:27 -0400

    Serializable supplier missing

--- conveyor-core/src/main/java/com/aegisql/conveyor/serial/SerializableSupplier.java
+++ conveyor-core/src/main/java/com/aegisql/conveyor/serial/SerializableSupplier.java
@@ -1,8 +1,9 @@
 package com.aegisql.conveyor.serial;
 
+import java.io.Serializable;
 import java.util.function.Supplier;
 
 @FunctionalInterface
-public interface SerializableSupplier<T> extends Supplier<T>{
+public interface SerializableSupplier<T> extends Supplier<T>, Serializable {
 
 }
